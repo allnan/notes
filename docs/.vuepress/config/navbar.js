49f75701c68f8277e09ab3c1a0945d47c562f0d5
all_nan <all_nan@hotmail.com>
2019-09-16 18:04:35 +0800

    preparre

--- docs/.vuepress/config/navbar.js
+++ docs/.vuepress/config/navbar.js
@@ -1,7 +1,22 @@
 module.exports = [
-    { text: "home", link: "/" },
-    { text: "dart", link: "/dart/" },
-    { text: "flutter", link: "/flutter/" },
-    { text: "java", link: "/java/" },
-    { text: "android", link: "/android/" },
-  ]
+  { text: "Home", link: "/" },
+  {
+    text: "Dart", items: [
+      {
+        text: "参考", items: [
+          { text: "Dart官网", link: "https://dart.dev/", },
+          { text: "Dart官网Tour", link: "https://dart.dev/guides/language/language-tour", },
+          { text: "三方翻译", link: "https://github.com/konieshadow/dart-tourY", },
+          { text: "", link: "", },
+        ],
+      },{
+        text: "学习", items: [
+            {text:"Dart官方Tour译读",link:"/dart/"}
+        ]
+      }
+    ]
+  },
+  { text: "flutter", link: "/flutter/" },
+  { text: "java", link: "/java/" },
+  { text: "android", link: "/android/" },
+]
