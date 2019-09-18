ee14f3ec5d47cee904019bf053b3b45f22b5e065
all_nan <all_nan@hotmail.com>
2019-09-18 23:44:55 +0800

    change name

--- docs/dart/sidebar.js
+++ docs/dart/sidebar.js
@@ -5,9 +5,9 @@ module.exports = [
         path:'/dart/官方Tour译读/',
         sidebarDepth: 2,
         children: [
-          '/dart/官方Tour译读/Dart官方教程译读 一 Hello world',
-          '/dart/官方Tour译读/Dart官方教程译读 二 变量',
-          '/dart/官方Tour译读/Dart官方教程译读 三 内置类型',
+          '/dart/官方Tour译读/一 Hello world',
+          '/dart/官方Tour译读/二 变量',
+          '/dart/官方Tour译读/三 内置类型',
         ]
     }
 ]
